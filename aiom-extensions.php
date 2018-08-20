a312a58b89ef5aace0cf3573ef59f1288a5060f0
Zhitnitskii Daniil <danonchik98@gmail.com>
2018-08-20 10:45:18 +0200

    main file header

---
+++ aiom-extensions.php
@@ -0,0 +1,9 @@
+/*
+Plugin Name: AIOM Extensions
+Plugin URI: https://github.com/my-language-skills/aiom-extensions
+Description: This plugin provides auto-generated metadata on the basis of default WP web-pages information. With us of All-In-One Metadata plugin provides extended functionality.
+Version: 0.1
+Author: Daniil Zhitnitskii (My Language Skills)
+Author URI: https://github.com/my-language-skills
+License: GPL 3.0
+*/
