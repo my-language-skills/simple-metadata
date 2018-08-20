3e008236b18e85f43ee11185738cff5236c2a86a
Zhitnitskii Daniil <danonchik98@gmail.com>
2018-08-20 16:37:23 +0200

    functionality for 'page' type

--- aiom-extensions.php
+++ aiom-extensions.php
@@ -1,9 +1,14 @@
+<?php
 /*
 Plugin Name: AIOM Extensions
 Plugin URI: https://github.com/my-language-skills/aiom-extensions
-Description: This plugin provides auto-generated metadata on the basis of default WP web-pages information. With us of All-In-One Metadata plugin provides extended functionality.
+Description: This plugin provides auto-generated metadata on the basis of default WP web-pages information. With use of All-In-One Metadata plugin provides extended functionality.
 Version: 0.1
 Author: Daniil Zhitnitskii (My Language Skills)
 Author URI: https://github.com/my-language-skills
 License: GPL 3.0
 */
+
+defined ("ABSPATH") or die ("No script assholes!");
+
+include_once plugin_dir_path( __FILE__ ) . "core-functionality/core-functionality.php";
---
+++ core-functionality/core-functionality.php
@@ -0,0 +1,124 @@
+<?php
+
+/**
+ *	Default functionality of plugin
+ */
+
+defined ("ABSPATH") or die ("No script assholes!");
+
+/**
+ *	Function for creation of metabox to pick type of page for proper Schema.org schema type
+ */
+function aiex_add_page_type_meta () {
+	if (current_user_can('administrator')){
+		add_meta_box (
+			'aiex_page_type', //Unique ID
+			'Page Type', //Title
+			'aiex_render_page_type_meta', //Callback function
+			'page', //for pages
+			'side', //Context
+			'high' //priority
+		);
+	}
+}
+
+function aiex_render_page_type_meta ($object, $box) {
+	//creating nonce
+	wp_nonce_field( basename( __FILE__ ), 'aiex_render_page_type_meta' );
+
+	$page_type = esc_attr(get_post_meta ($object->ID, 'aiex_page_type', true));
+	$page_types = array(
+					'no_page_type'		=> '--Select--',
+					'AboutPage' 		=> 'About Page',
+					'CheckoutPage' 		=> 'Checkout Page',
+					'CollectionPage' 	=> 'Collection Page',
+					'ContactPage'		=> 'Contact Page',
+					'FAQPage'			=> 'FAQ Page',
+					'ItemPage'			=> 'Item Page',
+					'MedicalWebPage'	=> 'Medical Web Page',
+					'ProfilePage'		=> 'Profile Page',
+					'QAPage'			=> 'QA Page',
+					'SearchResultsPage'	=> 'Search Results Page'
+				  );
+	?>
+		<p>Page Type</p>
+			<select style="width: 90%;" name="aiex_page_type" id="aiex_page_type">
+				<?php
+					foreach ($page_types as $key => $value) {
+						$selected = $page_type == $key ? 'selected' : '';
+						echo '<option value="'.$key.'" '.$selected.'>'.$value.'</option>';
+					}
+				?>
+			</select>
+	<?php
+}
+
+/**
+ * Function for post saving/updating action
+ */
+function aiex_save_page_type ($post_id, $post) {
+
+	/* Verify the nonce before proceeding. */
+	if ( !isset( $_POST['aiex_render_page_type_meta'] ) || !wp_verify_nonce( $_POST['aiex_render_page_type_meta'], basename( __FILE__ ) ) ){
+		return $post_id;
+	}
+
+
+	/* Get the post type object. */
+	$post_type = get_post_type_object( $post->post_type );
+
+	//if user is not administrator, exit function
+	if ( !current_user_can( 'administrator' ) ){
+		return $post_id;
+	}
+
+	//fetching old and new meta values if they exist
+	$new_meta_value = isset($_POST['aiex_page_type']) ? sanitize_text_field ($_POST['aiex_page_type']) : '';
+	$old_meta_value = get_post_meta ($post_id, 'aiex_page_type', true);
+
+	if ( $new_meta_value && '' == $old_meta_value && $new_meta_value != 'no_page_type' ) {
+		add_post_meta( $post_id, 'aiex_page_type', $new_meta_value, true ); 
+	} elseif ( $new_meta_value && $new_meta_value != $meta_value && $new_meta_value != 'no_page_type' ) {
+		update_post_meta( $post_id, 'aiex_page_type', $new_meta_value );
+	} elseif ( 'no_page_type' == $new_meta_value && $old_meta_value ) {
+		delete_post_meta( $post_id, 'aiex_page_type', $old_meta_value );
+	}
+}
+
+/**
+ * Function responsible for output of metadata automatically obtained from page information
+ */
+function aiex_print_page_meta_fields () {
+
+	if ('page' == get_post_type(get_the_ID())) {
+		$page_type = get_post_meta(get_the_ID(), 'aiex_page_type', true) ?: 'no_page_type';
+		if ('no_page_type' == $page_type){
+			return;
+		}
+		$author = get_the_author_meta('first_name') && get_the_author_meta('last_name') ? get_the_author_meta('first_name').' '.get_the_author_meta('last_name') : get_the_author_meta('display_name', get_post_field('post_author', get_the_ID()));
+		$creation_date = get_the_date();
+		$title = get_the_title();
+		$last_modification_date = get_the_modified_date();
+		$last_modifier = get_the_modified_author();
+		$thumbnail_url = get_the_post_thumbnail_url();
+		$publication_date = get_the_time(get_option( 'date_format' ));;
+		?>
+
+		<div itemscope itemtype="http://schema.org/<?=$page_type;?>">
+			<meta itemprop="author" content="<?=$author;?>">
+			<meta itemprop="dateCreated" content="<?=$creation_date;?>">
+			<meta itemprop="headline" content="<?=$title;?>">
+			<meta itemprop="lastReviewed" content="<?=$last_modification_date;?>">
+			<meta itemprop="editor" content="<?=$last_modifier;?>">
+			<meta itemprop="thumbnailUrl" content="<?=$thumbnail_url;?>">
+			<meta itemprop="datePublished" content="<?=$publication_date?>">
+		</div>
+
+		<?php
+	}
+
+}
+
+add_action ('add_meta_boxes', 'aiex_add_page_type_meta');
+add_action ('save_post', 'aiex_save_page_type', 10, 2);
+add_action ('wp_head', 'aiex_print_page_meta_fields');
