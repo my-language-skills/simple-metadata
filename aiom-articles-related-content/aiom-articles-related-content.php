99c08878db804f829f73c4871fd790dcac1bd9ef
Zhitnitskii Daniil <danonchik98@gmail.com>
2018-08-27 11:33:09 +0200

    articles extension

--- aiom-articles-related-content/aiom-articles-related-content.php
+++ aiom-articles-related-content/aiom-articles-related-content.php
@@ -12,3 +12,126 @@ License: GPL 3.0
 
 
 defined ("ABSPATH") or die ("No script assholes!");
+
+/**
+ *	Function for creation of metabox to pick type of news post for proper Schema.org schema type
+ */
+function aiex_add_article_post_type_meta () {
+
+	add_meta_box (
+		'aiex_article_post_type', //Unique ID
+		'Type Of Article', //Title
+		'aiex_render_article_post_type_meta', //Callback function
+		'post', //for pages
+		'side', //Context
+		'high' //priority
+	);
+}
+
+function aiex_render_article_post_type_meta ($object, $box) {
+	//creating nonce
+	wp_nonce_field( basename( __FILE__ ), 'aiex_render_article_post_type_meta' );
+
+	$article_type = esc_attr(get_post_meta ($object->ID, 'aiex_article_post_type', true));
+	$article_types = array(
+					'no_type'					=> '--Select--'
+					'Report'					=> 'Report',
+					'SatiricalArticle' 			=> 'Satirical Article',
+					'ScholarlyArticle' 			=> 'Scholarly Article',
+					'MedicalScholarlyArticle' 	=> 'Medical Scholarly Article',
+					'SocialMediaPosting'		=> 'Social Media Posting',
+					'BlogPosting'				=> 'Blog Posting',
+					'DiscussionForumPosting'	=> 'Discussion Forum Posting',
+					'TechArticle'				=> 'Technology Article',
+				  );
+	?>
+		<p>Article Post Type</p>
+			<select style="width: 90%;" name="aiex_article_post_type" id="aiex_article_post_type">
+				<?php
+					foreach ($article_types as $key => $value) {
+						$selected = $article_type == $key ? 'selected' : '';
+						echo '<option value="'.$key.'" '.$selected.'>'.$value.'</option>';
+					}
+				?>
+			</select>
+	<?php
+}
+
+/**
+ * Function for post saving/updating action
+ */
+function aiex_save_article_post_type ($post_id, $post) {
+
+	/* Verify the nonce before proceeding. */
+	if ( !isset( $_POST['aiex_render_article_post_type_meta'] ) || !wp_verify_nonce( $_POST['aiex_render_article_post_type_meta'], basename( __FILE__ ) ) ){
+		return $post_id;
+	}
+
+
+	//fetching old and new meta values if they exist
+	$new_meta_value = isset($_POST['aiex_article_post_type']) ? sanitize_text_field ($_POST['aiex_article_post_type']) : '';
+	$old_meta_value = get_post_meta ($post_id, 'aiex_article_post_type', true);
+
+	if ( $new_meta_value && '' == $old_meta_value && $new_meta_value != 'no_type' ) {
+		add_post_meta( $post_id, 'aiex_article_post_type', $new_meta_value, true ); 
+	} elseif ( $new_meta_value && $new_meta_value != $meta_value && $new_meta_value != 'no_type' ) {
+		update_post_meta( $post_id, 'aiex_article_post_type', $new_meta_value );
+	} elseif ( 'no_type' == $new_meta_value && $old_meta_value ) {
+		delete_post_meta( $post_id, 'aiex_article_post_type', $old_meta_value );
+	}
+}
+
+function aiex_print_article_post_meta_fields () {
+
+	if ('post' == get_post_type(get_the_ID())) {
+
+		$article_type = get_post_meta(get_the_ID(), 'aiex_article_post_type', true) ?: 'empty';
+
+		if ('empty' == $news_type){
+			return;
+		}
+
+		$post_id = get_the_ID();
+
+		
+		$post_content = get_post( $post_id )->post_content;
+		$word_count = str_word_count($post_content);
+		$categories = get_the_category( $post_id);
+		$categories_arr = [];
+		foreach ($categories as $category) {
+			$categories_arr[] = $category->name;
+		}
+		$categories_string = implode(', ', $categories_arr);
+		$key_words = wp_get_post_tags($post_id, ['fields' => 'names']);
+		$key_words_string = implode(', ', $key_words);
+
+		$author_id = get_post_field('post_author', $post_id);
+		$author = get_the_author_meta('first_name', $author_id) && get_the_author_meta('last_name', $author_id) ? get_the_author_meta('first_name', $author_id).' '.get_the_author_meta('last_name', $author_id) : get_the_author_meta('display_name', $author_id);
+		$creation_date = get_the_date();
+		$title = get_the_title();
+		$last_modifier = get_the_modified_author();
+		$thumbnail_url = get_the_post_thumbnail_url();
+		$publication_date = get_the_time(get_option( 'date_format' ));;
+		?>
+
+		<div itemscope itemtype="http://schema.org/<?=$article_type;?>">
+			<meta itemprop="author" content="<?=$author;?>">
+			<meta itemprop="dateCreated" content="<?=$creation_date;?>">
+			<meta itemprop="headline" content="<?=$title;?>">
+			<meta itemprop="editor" content="<?=$last_modifier;?>">
+			<meta itemprop="thumbnailUrl" content="<?=$thumbnail_url;?>">
+			<meta itemprop="image" content="<?=$thumbnail_url;?>">
+			<meta itemprop="datePublished" content="<?=$publication_date?>">
+			<meta itemprop="keywords" content="<?=$key_words_string?>">
+			<meta itemprop="articleSection" content="<?=$categories_string?>">
+			<meta itemprop="wordCount" content="<?=$word_count?>">
+		</div>
+
+		<?php
+	}
+
+}
+
+add_action ('add_meta_boxes', 'aiex_add_article_post_type_meta');
+add_action ('save_post', 'aiex_save_article_post_type', 10, 2);
+add_action ('wp_head', 'aiex_print_article_post_meta_fields');
