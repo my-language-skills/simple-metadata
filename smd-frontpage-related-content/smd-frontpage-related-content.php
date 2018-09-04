b621418898d81fcfa1bc36d946b8521ebb56608d
Zhitnitskii Daniil <danonchik98@gmail.com>
2018-09-04 15:31:57 +0200

    settings page

--- smd-frontpage-related-content/smd-frontpage-related-content.php
+++ smd-frontpage-related-content/smd-frontpage-related-content.php
@@ -6,11 +6,40 @@
 /**
  * Fuction for creating option to choose etween blog and web-site
  */
-function smd_add_radio_option () {
-	register_setting ('reading', 'smd_website_blog_type');
-	add_settings_field ('smd_website_blog_type', 'Type of Site', 'smd_render_switch_set', 'reading', 'default');
+function smd_add_option_page () {
+	register_setting ('smd_set_page', 'smd_website_blog_type');
+	add_menu_page('Simple Metadata', 'Simple Metadata', 'manage_options', 'smd_set_page', 'smd_render_options_page', 'dashicons-search');
+	add_settings_section( 'smd_set_page', '', '', 'smd_set_page' );
+	add_settings_field ('smd_website_blog_type', 'Type of Site', 'smd_render_switch_set', 'smd_set_page', 'smd_set_page');
 }
 
+
+/**
+ * Render the options page for plugin.
+ *
+ * @since  1.0
+ */
+function smd_render_options_page() {
+	if(!current_user_can('manage_options')){
+		return;
+	}
+	?>
+       <div class="wrap">
+           <h2>Simple Metadata Settings</h2>
+           <form method="post" action="options.php">
+			<?php
+			settings_fields( 'smd_set_page' );
+			do_settings_sections( 'smd_set_page' );
+			submit_button();
+			?>
+		   </form>
+       </div>
+	<?php
+}
+
+/**
+ * Function for rendering radio button
+ */
 function smd_render_switch_set() {
 	?>
 	<label for="smd_website_blog_type_1">Blog <input type="radio" id="smd_website_blog_type_1" name="smd_website_blog_type" value="Blog" <?php checked('Blog', get_option('smd_website_blog_type'))?>></label>
@@ -45,5 +74,5 @@ function smd_print_wsb_field () {
 }
 
 
-add_action ('admin_init', 'smd_add_radio_option');
+add_action ('admin_menu', 'smd_add_option_page');
 add_action ('wp_head', 'smd_print_wsb_field');
