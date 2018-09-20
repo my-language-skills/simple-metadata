72ae7c477ba9656d6d2f70748eeca82e9f092d70
Zhitnitskii Daniil <danonchik98@gmail.com>
2018-09-20 16:26:27 +0200

    increased modularity, reorganized settings

--- smd-posts-related-content/smd-posts-related-content.php
+++ smd-posts-related-content/smd-posts-related-content.php
@@ -153,114 +153,27 @@ function smd_print_post_meta_fields () {
 				break;
 			}
 		}
+
+		//> data, related to 'Article' type properties (only applicable for 'post' and post-compitable CPTs)
+
+			//keywords
+			$key_words = wp_get_post_tags($post_id, ['fields' => 'names']);
+			$key_words_string = implode(', ', $key_words);
+
+		//<	
 		
 		?>
 
 		<div itemscope itemtype="http://schema.org/<?=$post_meta_type;?>">
 			<?php echo smd_get_general_tags($post_meta_type);?>
+			<meta itemprop='keywords' content='$key_words_string'>
 		</div>
 
 		<?php
 	}
 
 }
-
-/**
- * Function for getting properties' metatags common for every type
- */
-function smd_get_general_tags($post_meta_type) {
-
-	$post_id = get_the_ID();
-
-	$post_content = get_post( $post_id )->post_content;
-	$word_count = str_word_count($post_content);
-
-	$key_words = wp_get_post_tags($post_id, ['fields' => 'names']);
-	$key_words_string = implode(', ', $key_words);
-
-	$author_id = get_post_field('post_author', $post_id);
-	$author = get_the_author_meta('first_name', $author_id) && get_the_author_meta('last_name', $author_id) ? get_the_author_meta('first_name', $author_id).' '.get_the_author_meta('last_name', $author_id) : get_the_author_meta('display_name', $author_id);
-	$creation_date = get_the_date();
-	$last_modification_date = get_the_modified_date();
-	$title = get_the_title();
-	$last_modifier = get_the_modified_author();
-	$thumbnail_url = get_the_post_thumbnail_url();
-
-	$categories = get_the_category( $post_id);
-	$categories_arr = [];
-	foreach ($categories as $category) {
-		$categories_arr[] = $category->name;
-	}
-	$categories_string = implode(', ', $categories_arr);
-
-	//getting logo image 
-	$custom_logo_id = get_theme_mod( 'custom_logo' );
-	$logo = isset(wp_get_attachment_image_src( $custom_logo_id , 'full' )[0]) ? wp_get_attachment_image_src( $custom_logo_id , 'full' )[0] : (get_avatar_url($author_id) ?: '');
-
-	$publisher = get_bloginfo();
-	$type = 'Organization';
-	$publication_date = get_the_time(get_option( 'date_format' ));
-
-	//include to check if YOAST or SEO Framework are installed
-	include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 		
-	//if YOAST plugin is active, get company or person name to set for publisher property
-	if (is_plugin_active('wordpress-seo/wp-seo.php')){
-
-		$all_vals = get_option('wpseo_titles');
-
-		if (!empty($all_vals)){
-
-			$publisher = 'company' == $all_vals['company_or_person'] ? $all_vals['company_name'] : ('person' == $all_vals['company_or_person'] ? $all_vals['person_name'] : get_bloginfo()) ;
-
-			if ($all_vals['company_logo'] && 'company' == $all_vals['company_or_person']){
-				$logo = $all_vals['company_logo'];
-			}
-		}
-	}
-
-	//if SEO Framework plugin is active, get company or person name to set for publisher property
-	if (is_plugin_active('autodescription/autodescription.php')){
-
-		$all_vals = get_option('autodescription-site-settings');
-
-		if (!empty($all_vals)){
-
-			$publisher = $all_vals['knowledge_name'] ?: get_bloginfo();
-
-			if ($all_vals['knowledge_logo_url'] && 'organization' == $all_vals['knowledge_type']){
-				$logo = $all_vals['knowledge_logo_url'];
-			}
-		}
-	}
-
-	$image = $thumbnail_url ?: $logo;
-
-	$html = "<meta itemprop='author' content='$author'>\n".
-			"<meta itemprop='dateCreated' content='$creation_date'>\n".
-			"<meta itemprop='headline' content='$title'>\n".
-			"<meta itemprop='editor' content='$last_modifier'>\n".
-			"<meta itemprop='thumbnailUrl' content='$thumbnail_url'>\n".
-			"<meta itemprop='image' content='".$image."'>\n".
-			"<meta itemprop='dateModified' content='$last_modification_date'>\n".
-			"<meta itemprop='datePublished' content='$publication_date'>\n".
-			"<meta itemprop='keywords' content='$key_words_string'>\n".
-			"<div itemprop='publisher' itemscope itemtype='http://schema.org/$type'>\n".
-				"<meta itemprop='name' content='$publisher'>\n".
-				"<meta itemprop='logo' content='$logo'>\n".
-			"</div>\n".
-			"<div itemprop='mainEntityOfPage' itemscope itemtype='http://schema.org/WebPage'></div>\n";
-
-	$supported_types = ['Article', 'AdvertiserContentArticle', 'BlogPosting', 'DiscussionForumPosting', 'LiveBlogPosting',	'Report', 'SatiricalArticle' , 'SocialMediaPosting', 'TechArticle'];
-
-	if(in_array($post_meta_type, $supported_types)){
-		$html .= "<meta itemprop='articleBody' content='$post_content'>\n".
-				"<meta itemprop='articleSection' content='$categories_string'>\n".
-				"<meta itemprop='wordCount' content='$word_count'>";
-	}
-
-	return $html;
-}			
 
 if (!is_plugin_active('pressbooks/pressbooks.php')){
 	add_action ('add_meta_boxes', 'smd_add_post_type_meta');
