f0d023b07a4d5741ecc9d23e52540f4af7322f09
Antonio Devís <colomett@gmail.com>
2019-06-17 13:01:49 +0200

    Metabox description

--- network-admin/smd-network-admin.php
+++ network-admin/smd-network-admin.php
@@ -103,7 +103,7 @@ function smd_render_network_settings(){
 function smd_network_render_metabox_schema_locations(){
 	?>
 	<div id="smd_network_meta_locations" class="smd_network_meta_locations">
-		<span class="description"><span class="description">Description for general network settings metabox</span></span>
+		<span class="description"><span class="description">Actiavte the post types where metadata will be available. If activate, site administrators can not deactivate.</span></span>
 		<form method="post" action="edit.php?action=smd_update_network_locations">
 			<?php
 			settings_fields( 'smd_network_meta_locations' );
@@ -122,7 +122,7 @@ function smd_network_render_metabox_schema_locations(){
 function smd_network_render_metabox_sites_type(){
 	?>
 	<div id="smd_network_meta_sites_type" class="smd_network_meta_sites_type">
-		<span class="description">Description for site network option metabox</span>
+		<span class="description">Select the Homepage schema type. If selected, site administrators can not modify.</span>
 		<form method="post" action="edit.php?action=smd_update_network_options">
 			<?php
 			settings_fields( 'smd_network_meta_sites_type' );
@@ -151,7 +151,7 @@ function smd_render_net_switch_set() {
 		<?php
 	}
 
-	echo '<br><span class="description">Select schema type which will be appplied for front-page metadata</span>';
+	echo '<br><span class="description">By default, blogs uses site configuration.</span>';
 }
 
 /**
