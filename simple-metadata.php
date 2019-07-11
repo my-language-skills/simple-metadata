57e03c89027f34c479b1eacb0ad665bbbfdd4f86
Antonio Devís <colomett@gmail.com>
2019-07-11 13:45:22 +0200

    annotation

--- simple-metadata.php
+++ simple-metadata.php
@@ -1,16 +1,29 @@
 <?php
+
 /**
- * Plugin Name: Simple Metadata
- * Plugin URI: https://github.com/my-language-skills/simple-metadata
- * Description: This plugin provides auto-generated metadata on the basis of default WP web-pages information.
- * Version: 1.3
- * Author: My Language Skills team
- * Author URI: https://github.com/my-language-skills
- * License: GPL 3.0
- * License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
- * Text Domain: simple-metadata
- * Domain Path: /languages
-*/
+ * Simple Metadata
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              https://github.com/my-language-skills/simple-metadata
+ * @since             0.1
+ * @package           simple-metadata
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Simple Metadata
+ * Plugin URI:        https://github.com/my-language-skills/simple-metadata
+ * Description:       This plugin provides auto-generated metadata on the basis of default WP web-pages information.
+ * Version:           1.3
+ * Author:            My Language Skills team
+ * Author URI:        https://github.com/my-language-skills/
+ * License:           GPL 3.0
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+ * Text Domain:       simple-metadata
+ * Domain Path:       /languages
+ */
 
 defined ("ABSPATH") or die ("No script assholes!");
 
